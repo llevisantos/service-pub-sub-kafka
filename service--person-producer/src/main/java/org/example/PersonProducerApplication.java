39a3a0a4a65ab3a597e1faf8769db0ce19cab9da
Levi Santos <llevicosta@gmail.com>
2023-11-02 21:00:46 -0300

    feature: initial implements producer kafka

---
+++ service--person-producer/src/main/java/org/example/PersonProducerApplication.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonProducerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PersonProducerApplication.class, args);
+    }
+}
---
+++ service--person-producer/src/main/java/org/example/external_interfaces/config/KakfaTopicCreatorExample.java
@@ -0,0 +1,32 @@
+package org.example.external_interfaces.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+
+@Configuration
+public class KakfaTopicCreatorExample {
+
+    public static final String TOPIC_NAME = "topic-person-info";
+    public static final int PARTITIONS = 10;
+    @Autowired
+    private KafkaProperties kafkaProperties;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        var props = new HashMap<String, Object>();
+        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        return new KafkaAdmin(props);
+    }
+
+    @Bean
+    public NewTopic newTopic() {
+        return new NewTopic(TOPIC_NAME, PARTITIONS, Short.valueOf("1"));
+    }
+}
