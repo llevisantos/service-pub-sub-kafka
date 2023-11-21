1acd517616560a12ebc7cd908462ddd34310c3b0
llevisantos <llevicosta@gmail.com>
2023-11-21 20:07:58 -0300

    feat: implements improvements

--- service--person-consumer/src/main/java/org/example/external_interfaces/config/KafkaListenerConfig.java
+++ service--person-consumer/src/main/java/org/example/external_interfaces/config/KafkaListenerConfig.java
@@ -3,6 +3,8 @@ package org.example.external_interfaces.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,9 +12,13 @@ import org.springframework.stereotype.Component;
 public class KafkaListenerConfig {
 
     @KafkaListener(topics = "${kafka.topic-name}", groupId = "${kafka.group-id}")
-    public void listen(String mensage) {
+    public void listen(String mensage,
+                       @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+                       @Header(KafkaHeaders.OFFSET) String offset) {
         log.info("Thread: {}", Thread.currentThread().getId());
+        log.info("Received topic: {}", topic);
         log.info("Received: {}", mensage);
+        log.info("Offiset: {}", offset);
     }
 
 }
